302ca750c735da68b6aa70f339f08444b511db81
Linus <me@linusx.dev>
2022-11-15 21:43:21 -0500

    add EncodingLab

--- src/Lab/EncodingLab.java
+++ src/Lab/EncodingLab.java
@@ -1,4 +1,51 @@
 package Lab;
 
+/**
+ * Encodes a String entered by a user.
+ *
+ * @author Linus Xiong
+ * @version 11/15/2022
+ */
+import java.util.Scanner;
+
 public class EncodingLab {
+
+    public static final String CAPITALS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    /**
+     * Method that returns a string that is rot13 encoded from the input string.
+     *
+     * @param input
+     *            The string to encode
+     * @return The input string with the rot13 algorithm applied to it
+     */
+    public static String rot13(String input) {
+        StringBuilder sb = new StringBuilder();
+        String[] text = input.split("");
+        for (int i = 0; i < input.length(); i++){
+            sb.append(text[i]);
+            if (Character.isLetter(text[i].charAt(0))){
+                int index = CAPITALS.indexOf(text[i]);
+                sb.deleteCharAt(i);
+                if ((index + 13) >= 26){
+                    char result = CAPITALS.charAt((index+13)%13);
+                    sb.insert(i, result);
+                }else {
+                    char result = CAPITALS.charAt(index+13);
+                    sb.insert(i, result);
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter a string to encode: ");
+        String input = keyboard.nextLine();
+        String encoded = rot13(input);
+        System.out.println("Your string encoded is  : " + encoded);
+        keyboard.close();
+    }
+
 }
